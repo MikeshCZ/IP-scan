cf0d4d990e469a7438acf55dff3756f21625de89
Michal Šára <2155397+MikeshCZ@users.noreply.github.com>
2022-04-12 12:54:24 +0200

    init

---
+++ IP_scan.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+CLS
+ipconfig /all
+ECHO.
+
+Rem INPUTS
+SET /p ARGUMENT=Enter type of test (P)ING or (A)RP:%=%
+IF "%ARGUMENT%"=="PING" SET /p SUBNET=Enter IP range without last station number (like: 192.168.1):%=%
+IF "%ARGUMENT%"=="PING" SET /p FROM=Scan from:%=%
+IF "%ARGUMENT%"=="PING" SET /p TO=Scan to:%=%
+IF "%ARGUMENT%"=="PING" SET /p SPEED=Ping Speed (ms):%=%
+ECHO.
+
+ECHO #############################################################
+IF "%ARGUMENT%"=="P" GOTO PING
+IF "%ARGUMENT%"=="A" GOTO ARP
+GOTO END
+
+Rem PING method
+:PING
+  ECHO START SCANNING %SUBNET%.%FROM% - %TO% (%SPEED% ms per ping)
+  ECHO %0 > ipaddresses.txt
+  DATE /T >> ipaddresses.txt
+  TIME /T >> ipaddresses.txt
+  FOR /L %%i IN (%FROM%,1,%TO%) DO ping -n 1 -w %SPEED% %SUBNET%.%%i | FIND /i "Reply" >> ipaddresses.txt
+  ECHO.
+  type ipaddresses.txt
+  GOTO END
+
+Rem ARP method
+:ARP
+  ECHO ARP
+  ARP -a > ipaddresses.txt
+  type ipaddresses.txt
+  GOTO END
+
+Rem Done
+:END
+  ECHO.
+  ECHO DONE %0
+  ECHO #############################################################
+  ECHO.
+  PAUSE
